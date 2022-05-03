5d59a664b73ac313562a97eec37ba081d49ae907
PHuhn <PhilHuhn@yahoo.com>
2022-05-03 14:15:10 -0400

    V13.4.0.4, updated packages

--- net-incident/GetAngularVer.bat
+++ net-incident/GetAngularVer.bat
@@ -2,5 +2,6 @@
 @REM Note VER is reserved word
 @SET VRSN=8.2.14
 @SET VRSN=latest
-npm install --save-dev @angular-devkit/build-angular@%VRSN% @angular/cli@%VRSN% @angular/animations@%VRSN% @angular/common@%VRSN% @angular/compiler@%VRSN% @angular/compiler-cli@%VRSN% @angular/core@%VRSN% @angular/forms@%VRSN% @angular/platform-browser@%VRSN% @angular/platform-browser-dynamic@%VRSN% @angular/router@%VRSN% @angular/cdk@%VRSN%
+rem @angular-devkit/build-angular@%VRSN% 
+npm install --save-dev @angular/core@%VRSN% @angular/cli@%VRSN% @angular/common@%VRSN% @angular/compiler@%VRSN% @angular/compiler-cli@%VRSN% @angular/forms@%VRSN% @angular/platform-browser@%VRSN% @angular/animations@%VRSN% @angular/platform-browser-dynamic@%VRSN% @angular/router@%VRSN% @angular/cdk@%VRSN%
 pause
