ab0df9df3e250e926145ef205ecd607812b05976
PHuhn <PhilHuhn@yahoo.com>
2024-03-15 15:58:07 -0400

    V8.4.2.15, added toggle bootstrap themes

---
+++ NSG.NetIncident4.Core/wwwroot/js/switch-theme.js
@@ -0,0 +1,34 @@
+﻿/*
+** ===========================================================================
+** A collection of functions as follows:
+** . nsg_isDarkTheme		boolean value is the current theme light or dark
+** . nsg_switchTheme		return string of YYYY-MM-DD format of a date
+**
+** Includes various constants:
+** . _nsg_isDarkTheme		boolean value
+*/
+let _nsg_isDarkTheme = false;
+/**
+** Change the 'data-bs-theme' value in the index.html
+** to different light or dark theme.
+*/
+function nsg_switchTheme() {
+	const _colorTheme = document.getElementById('body-color-theme');
+	if (_colorTheme !== null) {
+		const _theme = !_nsg_isDarkTheme ? 'dark' : 'light';
+		_colorTheme.setAttribute('data-bs-theme', _theme);
+		console.log(`nsg_switchTheme: New color theme: ${_theme}`);
+		_nsg_isDarkTheme = !_nsg_isDarkTheme;
+		const _icon = document.getElementById('switch-theme-icon');
+		if (_icon) {
+			_icon.className = _theme === 'dark' ? 'fa fa-moon' : 'fa fa-sun';
+			return 0;
+		}
+		console.warn(`nsg_switchTheme: Failed to find: 'switch-theme-icon'`);
+		return 1;
+	}
+	console.warn(`nsg_switchTheme: Failed to find: 'body-color-theme'`);
+	return 2;
+}
+//
+// ===========================================================================
---
+++ NSG.NetIncident4.Core/wwwroot/lib/font-awesome/Readme-4-7-0.txt
@@ -0,0 +1,3 @@
+This is font awesome 4.7.0
+I downloaded zip and placed it in the lib folder.
+I removed the less and scss folders.
