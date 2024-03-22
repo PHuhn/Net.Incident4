de6ed1e2ee4c0fffbbc27f749f3b0016f5670942
PHuhn <PhilHuhn@yahoo.com>
2024-03-22 10:46:51 -0400

    V8.4.2.16 fixed theme toggle

--- NSG.NetIncident4.Core/wwwroot/js/switch-theme.js
+++ NSG.NetIncident4.Core/wwwroot/js/switch-theme.js
@@ -1,34 +1,72 @@
 ﻿/*
 ** ===========================================================================
 ** A collection of functions as follows:
-** . nsg_isDarkTheme		boolean value is the current theme light or dark
+** . nsg_toggleThemeIcon	match the icon to the selected light or dark theme
 ** . nsg_switchTheme		return string of YYYY-MM-DD format of a date
-**
-** Includes various constants:
-** . _nsg_isDarkTheme		boolean value
+** . nsg_changeTheme        change the theme [data-bs-theme] value
+** . nsg_toggleTheme        toggle the current theme
+** . nsg_getTheme           returns current theme in local storage
+** . init                   initialization steps
 */
-let _nsg_isDarkTheme = false;
-/**
-** Change the 'data-bs-theme' value in the index.html
-** to different light or dark theme.
-*/
-function nsg_switchTheme() {
-	const _colorTheme = document.getElementById('body-color-theme');
-	if (_colorTheme !== null) {
-		const _theme = !_nsg_isDarkTheme ? 'dark' : 'light';
-		_colorTheme.setAttribute('data-bs-theme', _theme);
-		console.log(`nsg_switchTheme: New color theme: ${_theme}`);
-		_nsg_isDarkTheme = !_nsg_isDarkTheme;
-		const _icon = document.getElementById('switch-theme-icon');
-		if (_icon) {
-			_icon.className = _theme === 'dark' ? 'fa fa-moon' : 'fa fa-sun';
-			return 0;
-		}
-		console.warn(`nsg_switchTheme: Failed to find: 'switch-theme-icon'`);
-		return 1;
-	}
-	console.warn(`nsg_switchTheme: Failed to find: 'body-color-theme'`);
-	return 2;
-}
-//
+$(function () {
+    /**
+    ** Change the font awesome from sun to moon and back to sun to match the
+    ** current theme.
+    ** @param {any/string} theme
+    ** @returns
+    */
+    function nsg_toggleThemeIcon(theme) {
+        const _methodName = 'nsg_toggleThemeIcon';
+        const _icon = document.getElementById('switch-theme-icon');
+        if (_icon) {
+            _icon.className = theme === 'light' ? 'fa fa-sun' : 'fa fa-moon';
+            return 0;
+        }
+        console.warn(`${_methodName}: Failed to find: 'switch-theme-icon'`);
+    }
+    /**
+    ** 
+    ** @param {any/string} theme
+    */
+    function nsg_changeTheme(theme) {
+        window.localStorage.setItem('theme', theme);
+        document.getElementsByTagName('body')[0].setAttribute('data-bs-theme', theme);
+        nsg_toggleThemeIcon(theme);
+    }
+    /**
+    ** Toggle the theme from light to dark and back again.
+    */
+    function nsg_toggleTheme() {
+        nsg_getTheme() == 'light' ? nsg_changeTheme('dark') : nsg_changeTheme('light');
+    }
+    /**
+    ** Get the current theme from local storage
+    ** @returns
+    */
+
+    function nsg_getTheme() {
+        return window.localStorage.getItem('theme') ?? 'dark';
+    }
+    /**
+    ** Force the initialization of the current theme with the following steps.
+    */
+    function init() {
+        let theme = nsg_getTheme();
+        if (theme) {
+            nsg_changeTheme(theme);
+        }
+    }
+    /**
+    ** Apply the click event to the toggle button.
+    */
+    document.getElementById('switch-theme-button').addEventListener(
+        'click', () => {
+            nsg_toggleTheme();
+        }
+    );
+    /**
+    ** Force the initialization of the current theme.
+    */
+    init();
+});
 // ===========================================================================
