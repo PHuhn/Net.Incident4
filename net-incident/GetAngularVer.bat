44355b4ad86eea4dcd00c9d7da9bd995d091611b
phuhn <PhilHuhn@yahoo.com>
2025-12-01 19:48:09 -0500

    npm audit fix

--- net-incident/GetAngularVer.bat
+++ net-incident/GetAngularVer.bat
@@ -1,7 +1,7 @@
 @REM GetAngularVer
 @REM Note VER is reserved word
-@SET VRSN=8.2.14
+@REM VRSN=20.3.13
 @SET VRSN=latest
-rem @angular-devkit/build-angular@%VRSN% 
+@rem @angular-devkit/build-angular@%VRSN% 
 npm install --save-dev @angular/core@%VRSN% @angular/cli@%VRSN% @angular/common@%VRSN% @angular/compiler@%VRSN% @angular/compiler-cli@%VRSN% @angular/forms@%VRSN% @angular/platform-browser@%VRSN% @angular/animations@%VRSN% @angular/platform-browser-dynamic@%VRSN% @angular/router@%VRSN% @angular/cdk@%VRSN% @angular-devkit/build-angular@%VRSN% --force
 pause
